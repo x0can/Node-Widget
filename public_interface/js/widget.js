8f06bc194c0afc80b7ec7efd4ed0a93f001e010a
alexmwaura <alexmwaura43@gmail.com>
2021-12-07 05:33:46 +0300

    set up public facing server

---
+++ public_interface/js/widget.js
@@ -0,0 +1,13 @@
+/* eslint-disable strict */
+/* eslint-disable no-unused-expressions */
+
+(function Widget () {
+  'use strict'
+  document.addEventListener('DOMContentLoaded', ready, false)
+
+  // **********************************
+
+  function ready () {
+    console.log('ready')
+  }
+})()
